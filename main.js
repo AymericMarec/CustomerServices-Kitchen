aa344e82e9a826d032198972fae1b985f6a86c07
Maymeric <aymeric.marec@gmail.com>
2025-04-10 10:35:30 +0200

    init project

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.whenReady().then(() => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        // fullscreen: true,
+        webPreferences: {
+            contextIsolation: true, 
+            nodeIntegration: false,
+            sandbox: false,
+        }
+    });
+
+    mainWindow.loadFile('./view/home.html');
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
